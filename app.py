c53d5b57f9b1b34374748d3c2bd024b957275c9d
Watercrest28 <salomon.ibarra01@utrgv.edu>
2022-11-15 13:38:38 -0600

    draft

---
+++ app.py
@@ -0,0 +1,22 @@
+# app.py file for Flask app
+
+from flask import Flask,render_template
+
+# Create the app
+app = Flask(__name__)
+
+# Create a homepage for the app
+@app.route("/")
+# When we go to our.URL.com/
+    # Then flask will run this function below
+
+    # In our function, we will "return" 
+    # the HTML that we want flask to serve
+def home_page():
+    return render_template("base.html")
+
+@app.route("/user/Herbert")
+
+def about_page():
+    return 'hello,Herbert'
+
